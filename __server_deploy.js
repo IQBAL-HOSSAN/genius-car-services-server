bda70cea4bab233d8bdb58aba7dbd1c7818724e4
Iqbal Hossan <engiqbal1999@gmail.com>
2022-04-27 16:18:28 +0600

    check steps

---
+++ __server_deploy.js
@@ -0,0 +1,11 @@
+/* -----------------------------
+    ONE TIME for your computer
+    ----------------------------
+    1. create heroku account
+    2. verify email
+    3. install heroku cli to type heroku login
+    4. heroku login
+    ----------------------------
+    For each projects one time
+    -----------------------------
+*/
